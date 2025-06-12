096c75a76cc99d35ce080b3a15d287332910c526
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-06-12 14:42:40 -0700

    Document the example function

--- R/mean_difference.R
+++ R/mean_difference.R
@@ -1,3 +1,15 @@
+#' Calculate mean difference
+#'
+#' @param x A numeric vector.
+#' @param y A numeric vector.
+#'
+#' @returns A single numeric value.
+#' @export
+#'
+#' @examples
+#' x <- c(1, 2, 3)
+#' y <- c(4, 5, 6)
+#' mean_difference(x, y)
 mean_difference <- function(x, y) {
   abs(mean(x) - mean(y))
 }

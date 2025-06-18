e58b802c56eec0f9dae074e32c1ba147acb73cfc
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-06-17 17:58:47 -0700

    Move helper function out of filter_aov To do: get_score, calc_score

--- R/filter-aov.R
+++ R/filter-aov.R
@@ -3,7 +3,8 @@ filter_aov <- function(
   trans = NULL,
   direction = "maximize"
 ) {
-  new_cat_num_score(
+  new_filters_score(
+    subclass = c("any"),
     outcome_type = c("numeric", "factor"),
     predictor_type = c("numeric", "factor"),
     case_weights = case_weights,
@@ -11,32 +12,21 @@ filter_aov <- function(
     inclusive = c(TRUE, TRUE),
     fallback_value = Inf,
     score_type = score_type,
-    trans = trans,
-    sorts = sorts,
+    trans = NULL, # To do
+    sorts = NULL, # To do
     direction = direction,
     deterministic = TRUE,
     tuning = FALSE,
     ties = ties,
+    calculating_fn = get_f_stat,
     label = c(filter_aov = "Filter method based on ANOVA F-statistics")
   )
 }
 
-get_f_stat <- function(x, y) {
-  # Helper function to check if a vector is a factor with 2+ levels
-  is_factor <- function(v) is.factor(v) && length(levels(v)) >= 2
+# To do: Helper function to check if a vector is a factor with 2+ levels
+# flip x, y if needed, e.g., lm(y~x) and lm(x~y). Pass to get_f_stat.
 
-  if (is_factor(y) && is.numeric(x)) {
-    outcome <- x
-    predictor <- y
-  } else if (is.numeric(y) && is_factor(x)) {
-    outcome <- y
-    predictor <- x
-  } else {
-    stop(
-      "One argument must be a factor with 2+ levels and the other numeric."
-    )
-  }
-
+get_f_stat <- function(predictor, outcome) {
   if (length(levels(outcome)) == 2) {
     fit <- lm(outcome ~ predictor)
     res <- summary(fit)$fstatistic[1]
@@ -48,7 +38,7 @@ get_f_stat <- function(x, y) {
 }
 
 # To do:
-# Need a for loop or map here to handle all predictor-outcome pairs
+# Need a for loop or map to handle all predictor-outcome pairs
 
 # To do:
 # get_p_val <- function() {

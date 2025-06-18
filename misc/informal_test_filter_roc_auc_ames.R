63abb9036e726f122a4f03dcc61bad235cb752ef
Frances Lin <franceslinyc@gmail.com>
2025-06-17 20:26:46 -0700

    Add cases for filter methods for roc aoc (#4)
    * Add informal test for filter roc auc for flipped x, y
    
    * Add flip_if_needed and modify get_roc_auc to flip x, y if needed, e.g., f(x, y) and f(y,x) for roc and multiclass.roc

---
+++ misc/informal_test_filter_roc_auc_ames.R
@@ -0,0 +1,37 @@
+# Two class example
+data(ames, package = "modeldata")
+ames |> str()
+outcome <- ames$Sale_Price
+predictor <- ames$Street
+
+roc <- pROC::roc(predictor, outcome, direction = "auto")
+res <- pROC::auc(roc)
+
+# Multi class example
+outcome <- ames$Sale_Price
+predictor <- ames$MS_SubClass
+roc <- pROC::roc(predictor, outcome, direction = "auto")
+res <- pROC::auc(roc)
+
+outcome <- ames$Sale_Price
+predictor <- ames$MS_SubClass
+roc <- pROC::multiclass.roc(predictor, outcome, direction = "auto")
+res <- pROC::auc(roc)
+
+roc <- pROC::multiclass.roc(ames$MS_Zoning, outcome, direction = "auto")
+res <- pROC::auc(roc)
+
+# Test get_roc_auc
+outcome <- ames$Sale_Price
+predictor <- ames$Street
+get_roc_auc(predictor, outcome)
+get_roc_auc(ames$Central_Air, outcome)
+
+# Test get_all_roc_auc
+data <- ames
+outcome <- "Sale_Price"
+filter_obj = filter_roc_auc()
+tbl <- get_score(filter_obj, data, outcome)
+tbl
+
+# Test calc_score

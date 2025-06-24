1d654bcb4b20cfddf76a76578021338be0a38f7b
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-06-24 10:43:01 -0700

    Add test for partykit

--- tests/testthat/test-score-forest_imp.R
+++ tests/testthat/test-score-forest_imp.R
@@ -15,7 +15,7 @@ test_that("get_score_forest_importance() is working", {
   score_obj$trees <- 10
   score_obj$mtry <- 2
   score_obj$min_n <- 1
-  res <- get_score_forest_importance(score_obj, data, outcome)
+  res <- get_scores_forest_importance(score_obj, data, outcome)
 
   fit <- ranger::ranger(
     formula = class ~ .,
@@ -42,7 +42,51 @@ test_that("get_score_forest_importance() is working", {
   expect_equal(unique(res$outcome), "class")
 })
 
-# TODO Test partykit::cforest
 # TODO Test aorsf::orsf
 # TODO Test regression for ranger::ranger, set classification = FALSE
 # TODO Test more after we add validators
+
+test_that("get_score_forest_importance() is working for partykit", {
+  skip_if_not_installed("modeldata")
+  data(cells, package = "modeldata")
+  data <- tibble::tibble(
+    case = cells$case,
+    class = cells$class,
+    angle_ch_1 = cells$angle_ch_1,
+    area_ch_1 = cells$area_ch_1,
+    avg_inten_ch_1 = cells$avg_inten_ch_1,
+    avg_inten_ch_2 = cells$avg_inten_ch_2,
+  )
+  outcome <- "class"
+  score_obj <- score_forest_imp()
+  score_obj$engine <- "partykit"
+  score_obj$trees <- 10
+  score_obj$mtry <- 2
+  score_obj$min_n <- 1
+  res <- get_scores_forest_importance(score_obj, data, outcome)
+
+  set.seed(42)
+  fit <- partykit::cforest(
+    formula = class ~ .,
+    data = data,
+    control = ctree_control(minsplit = score_obj$min_n), # TODO Eventually have user pass in ctree_control()
+    ntree = score_obj$trees,
+    mtry = score_obj$mtry,
+  )
+  imp <- partykit::varimp(fit, conditional = TRUE)
+  predictors <- setdiff(names(data), outcome)
+  exp.imp <- as.numeric(imp[predictors])
+  exp.imp[is.na(exp.imp)] <- 0
+
+  expect_true(tibble::is_tibble(res))
+
+  expect_identical(nrow(res), ncol(data) - 1L)
+
+  expect_named(res, c("name", "score", "outcome", "predictor"))
+
+  expect_identical(res$score, exp.imp)
+
+  expect_equal(unique(res$name), "permutation")
+
+  expect_equal(unique(res$outcome), "class")
+})

f0eee7b2c5622d42d24f8d3273c308125d838651
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-06-25 10:23:33 -0700

    Add test for misc

--- R/misc.R
+++ R/misc.R
@@ -1,16 +1,12 @@
-# For creating new transformation object
 transform_abs <- scales::trans_new(
+  # TODO Put an issue to scales. Point to this file.
   name = "abs",
   transform = function(x) abs(x),
   inverse = function(x) x # dummy inverse
 )
-transform_abs$transform(-1)
-transform_abs$transform(c(-1, 1, 1))
 
 transform_neg_log10 <- scales::trans_new(
   name = "neg-log-10",
   transform = function(x) -log10(x),
   inverse = function(x) 10^(-x)
 )
-transform_neg_log10$transform(0.01)
-transform_neg_log10$transform(c(0.001, 0.01, 0.1))

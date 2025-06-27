8c6cef997285d82325ba4f8b71ba98580a0e7529
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-06-27 11:57:27 -0700

    Break get_scores_forest_importance() into three calls, i.e., get_forest_imp_ranger(), get_forest_imp_partykit(), get_forest_imp_aorsf()

--- tests/testthat/test-score-forest_imp.R
+++ tests/testthat/test-score-forest_imp.R
@@ -1,6 +1,6 @@
 test_that("get_score_forest_importance() is working for ranger for classification", {
   skip_if_not_installed("modeldata")
-  data(cells, package = "modeldata") # FYI modeldata::cells() works too
+  data(cells, package = "modeldata")
   data <- modeldata::cells |>
     dplyr::select(
       case, # FYI Is not a predictor. Designate whether the row was in the training or test set in the original reference.
@@ -29,7 +29,7 @@ test_that("get_score_forest_importance() is working for ranger for classificatio
     importance = "permutation",
     min.node.size = 1,
     classification = TRUE,
-    seed = 42 # TODO Add this to pass tests. Remove later.
+    seed = 42 # TODO Add this to pass tests. Remove later. set.seed(42) alone does not seem to work.
   )
   exp.res <- unname(fit$variable.importance)
 
@@ -69,15 +69,18 @@ test_that("get_score_forest_importance() is working for ranger regression", {
   res <- get_scores_forest_importance(score_obj, data, outcome)
 
   set.seed(42)
+  y <- data[[outcome]]
+  X <- data[setdiff(names(data), outcome)]
   fit <- ranger::ranger(
-    formula = Sale_Price ~ .,
+    x = X, # Remove formula = Sale_Price ~ .,
+    y = y,
     data = data,
     num.trees = 10,
     mtry = 2,
     importance = "permutation",
     min.node.size = 1,
     classification = FALSE,
-    seed = 42 # TODO Add this to pass tests. Remove later.
+    seed = 42 # TODO Add this to pass tests. Remove later. set.seed(42) alone does not seem to work.
   )
   exp.res <- unname(fit$variable.importance)
 
@@ -141,7 +144,99 @@ test_that("get_score_forest_importance() is working for partykit classification"
   expect_equal(unique(res$outcome), "class")
 })
 
-# TODO Test partykit::cforest regression
+test_that("get_score_forest_importance() is working for partykit regression", {
+  skip_if_not_installed("modeldata")
+  data(ames, package = "modeldata")
+  data <- modeldata::ames |>
+    dplyr::select(
+      Sale_Price,
+      MS_SubClass,
+      MS_Zoning,
+      Lot_Frontage,
+      Lot_Area,
+      Street
+    )
+  outcome <- "Sale_Price"
+  score_obj <- score_forest_imp()
+  score_obj$engine <- "partykit"
+  score_obj$trees <- 10
+  score_obj$mtry <- 2
+  score_obj$min_n <- 1
+  set.seed(42)
+  res <- get_scores_forest_importance(score_obj, data, outcome)
+
+  set.seed(42)
+  fit <- partykit::cforest(
+    formula = Sale_Price ~ .,
+    data = data,
+    control = partykit::ctree_control(minsplit = 1), # TODO Eventually have user pass in ctree_control()
+    ntree = 10,
+    mtry = 2,
+  )
+  imp <- partykit::varimp(fit, conditional = TRUE)
+  predictors <- setdiff(names(data), outcome)
+  exp.imp <- as.numeric(imp[predictors])
+  exp.imp[is.na(exp.imp)] <- 0
+
+  expect_true(tibble::is_tibble(res))
+
+  expect_identical(nrow(res), ncol(data) - 1L)
+
+  expect_named(res, c("name", "score", "outcome", "predictor"))
+
+  expect_identical(res$score, exp.imp)
+
+  expect_equal(unique(res$name), "permutation")
+
+  expect_equal(unique(res$outcome), "Sale_Price")
+})
+
 # TODO Test aorsf::orsf classification
-# TODO Test aorsf::orsf regression
+
+test_that("get_score_forest_importance() is working for aorsf regression", {
+  skip_if_not_installed("modeldata")
+  data(ames, package = "modeldata")
+  data <- modeldata::ames |>
+    dplyr::select(
+      Sale_Price,
+      MS_SubClass,
+      MS_Zoning,
+      Lot_Frontage,
+      Lot_Area,
+      Street
+    )
+  outcome <- "Sale_Price"
+  score_obj <- score_forest_imp()
+  score_obj$engine <- "aorsf"
+  score_obj$trees <- 10
+  score_obj$mtry <- 2
+  set.seed(42)
+  res <- get_scores_forest_importance(score_obj, data, outcome)
+
+  set.seed(42)
+  fit <- aorsf::orsf(
+    formula = Sale_Price ~ .,
+    data = data,
+    n_tree = 10,
+    n_retry = 2,
+    importance = "permute"
+  )
+  imp <- fit$importance
+  predictors <- setdiff(names(data), outcome)
+  exp.imp <- as.numeric(imp[predictors])
+  exp.imp[is.na(exp.imp)] <- 0
+
+  expect_true(tibble::is_tibble(res))
+
+  expect_identical(nrow(res), ncol(data) - 1L)
+
+  expect_named(res, c("name", "score", "outcome", "predictor"))
+
+  expect_identical(res$score, exp.imp)
+
+  expect_equal(unique(res$name), "permutation")
+
+  expect_equal(unique(res$outcome), "Sale_Price")
+})
+
 # TODO Test more after we add validators

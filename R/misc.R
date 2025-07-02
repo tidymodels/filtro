e5cb00a5d9295546ff22dd09c7df31e8d298e97a
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-07-02 11:52:28 -0700

    #' @noRd #' @keywords internal to pass R CMD check

--- R/misc.R
+++ R/misc.R
@@ -16,11 +16,8 @@ transform_neg_log10 <- scales::trans_new(
   inverse = function(x) 10^(-x)
 )
 
-#' Check if target value `target` is supplied
-#'
-#' @param target NULL
-#'
-#' @export
+#' @noRd
+#' @keywords internal
 check_target <- function(target) {
   if (is.null(target)) {
     cli::cli_abort(
@@ -29,33 +26,24 @@ check_target <- function(target) {
   }
 }
 
-#' Check if number of predictors `num_terms` is specified
-#'
-#' @param num_terms NULL
-#'
-#' @export
+#' @noRd
+#' @keywords internal
 check_num_terms <- function(num_terms) {
   if (is.null(num_terms)) {
     cli::cli_abort("{.arg num_terms} must be specified")
   }
 }
 
-#' Check if proportion of predictors `prop_terms` is specified
-#'
-#' @param prop_terms NULL
-#'
-#' @export
+#' @noRd
+#' @keywords internal
 check_prop_terms <- function(prop_terms) {
   if (is.null(prop_terms)) {
     cli::cli_abort("{.arg prop_terms} must be specified")
   }
 }
 
-#' Check if cutoff value `cutoff` is specified
-#'
-#' @param cutoff NULL
-#'
-#' @export
+#' @noRd
+#' @keywords internal
 check_cutoff <- function(cutoff) {
   if (is.null(cutoff)) {
     cli::cli_abort("{.arg cutoff} must be specified")

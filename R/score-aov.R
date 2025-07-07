1f449e308b2b266f17168ca7e7ab3776f5ecc834
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-07-07 16:13:44 -0700

    Add details

--- R/score-aov.R
+++ R/score-aov.R
@@ -1,14 +1,25 @@
-#' Compute F-statistic and p-value scores using ANOVA F-test
+#' Create score object for ANOVA F-test F-statistics and p-values
 #'
-#' @param range NULL
+#' Create a score object that contains associated metadata such as `range`,
+#' `fallback_value`, `score_type`, `direction`, and other relevant attributes.
+#'
+#' @inheritParams new_score_obj
 #' @param trans NULL
-#' @param score_type NULL
-#' @param direction NULL
+#' @param score_type A character string indicating the type of scoring metric to compute.
+#' Available options include:
+#'    - `fstat`
+#'    - `pval`
+#' @param direction For F-statistics, the `direction` should be set to `"maximize"`.
+#' For p-values, since the default applies a negative log10 transformation to p-values,
+#' the `direction` should be set to `"maximize"`.
 #'
-#' @returns NULL
+#' @returns A score object containing associated metadata such as `range`,
+#' `fallback_value`, `score_type`, `direction`, and other relevant attributes.
+#'
 #' @export
 #'
-#' @examples NULL
+#' @examples
+#' score_aov()
 score_aov <- function(
   range = c(0, Inf),
   trans = NULL,
@@ -90,7 +101,44 @@ make_scores_aov <- function(score_type, score, outcome, predictors) {
   res
 }
 
+#' Compute F-statistic and p-value scores using ANOVA F-test
+#'
+#' Output a tibble result with with one row per predictor, and four columns:
+#' `name`, `score`, `predictor`, and `outcome`.
+#'
+#' @param score_obj A score object. See [score_aov()] for details.
+#'
+#' @param data A data frame or tibble containing the outcome and predictor variables.
+#' @param outcome A character string specifying the name of the outcome variable.
+#' @param neg_log10 A logical value, indicating whether to apply a negative log10
+#' transformation to p-values (`TRUE`) or not (`FALSE`).
+#' If `neg_log10` is `TRUE`, the transformation is applied, and `direction` should be
+#' set to `"maximize"`. If `neg_log10` is `FALSE`, raw p-values are used, and `direction`
+#' should be set to `"minimize"`. By default, `neg_log10` is set to `TRUE`.
+#'
+#' @return A tibble of result with one row per predictor, and four columns:
+#' - `name`: the name of scoring metric.
+#' - `score`: the score for the predictor-outcome pair.
+#' - `predictor`: the name of the predictor.
+#' - `outcome`: the name of the outcome.
+#'
 #' @export
+#'
+#' @examples
+#' data(ames, package = "modeldata")
+#' data <- modeldata::ames |>
+#'   dplyr::select(
+#'     Sale_Price,
+#'     MS_SubClass,
+#'     MS_Zoning,
+#'     Lot_Frontage,
+#'     Lot_Area,
+#'     Street
+#'   )
+#' outcome <- "Sale_Price"
+#' score_obj <- score_aov()
+#' score_res <- get_scores_aov(score_obj, data, outcome)
+#' score_res
 get_scores_aov <- function(score_obj, data, outcome) {
   if (score_obj$score_type == "fstat") {
     score_obj$calculating_fn <- get_single_f_stat
@@ -109,7 +157,7 @@ get_scores_aov <- function(score_obj, data, outcome) {
 
   if (
     score_obj$score_type == "pval" &&
-      (is.null(score_obj$neg_log_pval) || isTRUE(score_obj$neg_log_pval))
+      (is.null(score_obj$neg_log10) || isTRUE(score_obj$neg_log10))
   ) {
     score <- -log10(score)
   }

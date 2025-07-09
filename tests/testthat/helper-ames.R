88481729cc6e773a14515272f09d96f765ea5464
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-07-09 10:29:17 -0700

    Revise test

--- tests/testthat/helper-ames.R
+++ tests/testthat/helper-ames.R
@@ -1,5 +1,4 @@
-ames_score_obj <- function() {
-  skip_if_not_installed("modeldata")
+helper_ames <- function() {
   data <- modeldata::ames |>
     dplyr::select(
       Sale_Price,
@@ -9,9 +8,5 @@ ames_score_obj <- function() {
       Lot_Area,
       Street
     )
-  outcome <- "Sale_Price"
-  score_obj <- score_aov()
-  score_res <- get_scores_aov(score_obj, data, outcome)
-  score_obj <- score_obj |> attach_score(score_res)
-  score_obj
+  data
 }
--- tests/testthat/test-score-aov.R
+++ tests/testthat/test-score-aov.R
@@ -12,6 +12,7 @@ test_that("get_scores_aov() is working for fstat", {
     )
   outcome <- "Sale_Price"
   score_obj <- score_aov()
+
   score_res <- get_scores_aov(score_obj, data, outcome)
 
   fit <- stats::lm(ames$Sale_Price ~ ames$MS_SubClass)
@@ -49,25 +50,18 @@ test_that("get_scores_aov() is working for fstat", {
   expect_equal(unique(score_res$outcome), "Sale_Price")
 })
 
-# TODO Test Reversed stats::lm(x ~ y)
-# TODO Test -log10(pval)
+# TODO Test Reversed stats::lm(x ~ y) Can use same data
 
 test_that("get_scores_aov() is working for -log10(pval)", {
   skip_if_not_installed("modeldata")
-  data(ames, package = "modeldata")
-  data <- modeldata::ames |>
-    dplyr::select(
-      Sale_Price,
-      MS_SubClass,
-      MS_Zoning,
-      Lot_Frontage,
-      Lot_Area,
-      Street
-    )
-  outcome <- "Sale_Price"
-  score_obj <- score_aov()
-  score_obj$score_type <- "pval"
-  score_res <- get_scores_aov(score_obj, data, outcome)
+
+  ames_subset <- helper_ames()
+  score_obj <- score_aov(score_type = "pval") # TODO For others
+  score_res <- get_scores_aov(
+    score_obj,
+    data = ames_subset,
+    outcome = "Sale_Price"
+  )
 
   fit <- stats::lm(ames$Sale_Price ~ ames$MS_SubClass)
   exp.MS_SubClass <- -log10(stats::anova(fit)$`Pr(>F)`[1])
@@ -84,9 +78,13 @@ test_that("get_scores_aov() is working for -log10(pval)", {
 
   expect_true(tibble::is_tibble(score_res))
 
-  expect_identical(nrow(score_res), ncol(data) - 1L)
+  expect_identical(nrow(score_res), ncol(ames_subset) - 1L)
 
   expect_named(score_res, c("name", "score", "outcome", "predictor"))
+
+  expect_equal(unique(score_res$name), "pval")
+
+  expect_equal(unique(score_res$outcome), "Sale_Price")
 
   expect_identical(
     score_res$score,
@@ -98,10 +96,6 @@ test_that("get_scores_aov() is working for -log10(pval)", {
       exp.Street
     )
   )
-
-  expect_equal(unique(score_res$name), "pval")
-
-  expect_equal(unique(score_res$outcome), "Sale_Price")
 })
 
 test_that("get_scores_aov() is working for pval", {

b9cc5a43462d051f5116a2e4b9c1b2807d27fe71
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-07-11 13:47:38 -0700

    S7 cross tab

--- tests/testthat/test-score-cross_tab.R
+++ tests/testthat/test-score-cross_tab.R
@@ -1,27 +1,33 @@
 test_that("get_scores_cross_tab is working for -log10(chisq pval)", {
   library(titanic)
+
   titanic_train <- titanic_train %>%
     dplyr::mutate(dplyr::across(c(Survived, Pclass, Sex, Embarked), as.factor))
-  data <- titanic_train %>%
+  titanic_subset <- titanic_train %>%
     dplyr::select(Survived, Pclass, Sex, Age, Fare, Embarked)
-  outcome <- "Survived"
-  score_obj <- score_cross_tab()
-  score_obj$score_type <- "pval_chisq"
-  score_obj$fdr <- FALSE
-  score_res <- get_scores_cross_tab(score_obj, data, outcome)
 
-  exp.Pclass <- get_single_chisq(data$Pclass, data$Survived)
+  score_obj <- score_cross_tab(score_type = "pval_chisq")
+  score_res <- get_scores_cross_tab(
+    score_obj,
+    data = titanic_subset,
+    outcome = "Survived"
+  )
+
+  exp.Pclass <- get_single_chisq(titanic_subset$Pclass, titanic_subset$Survived)
   exp.Pclass <- -log10(exp.Pclass)
-  exp.Sex <- get_single_chisq(data$Sex, data$Survived)
+  exp.Sex <- get_single_chisq(titanic_subset$Sex, titanic_subset$Survived)
   exp.Sex <- -log10(exp.Sex)
   exp.Age <- NA
   exp.Fare <- NA
-  exp.Embarked <- get_single_chisq(data$Embarked, data$Survived)
+  exp.Embarked <- get_single_chisq(
+    titanic_subset$Embarked,
+    titanic_subset$Survived
+  )
   exp.Embarked <- -log10(exp.Embarked)
 
   expect_true(tibble::is_tibble(score_res))
 
-  expect_identical(nrow(score_res), ncol(data) - 1L)
+  expect_identical(nrow(score_res), ncol(titanic_subset) - 1L)
 
   expect_named(score_res, c("name", "score", "outcome", "predictor"))
 
@@ -37,26 +43,36 @@ test_that("get_scores_cross_tab is working for -log10(chisq pval)", {
 
 test_that("get_scores_cross_tab is working for chisq pval", {
   library(titanic)
+
   titanic_train <- titanic_train %>%
     dplyr::mutate(dplyr::across(c(Survived, Pclass, Sex, Embarked), as.factor))
-  data <- titanic_train %>%
+  titanic_subset <- titanic_train %>%
     dplyr::select(Survived, Pclass, Sex, Age, Fare, Embarked)
-  outcome <- "Survived"
-  score_obj <- score_cross_tab()
-  score_obj$score_type <- "pval_chisq"
-  score_obj$fdr <- FALSE
-  score_obj$neg_log_pval <- FALSE # Turn -log10() off
-  score_res <- get_scores_cross_tab(score_obj, data, outcome)
 
-  exp.Pclass <- get_single_chisq(data$Pclass, data$Survived)
-  exp.Sex <- get_single_chisq(data$Sex, data$Survived)
+  score_obj <- score_cross_tab(
+    score_type = "pval_chisq",
+    neg_log10 = FALSE,
+    direction = "minimize",
+    fallback_value = 0
+  )
+  score_res <- get_scores_cross_tab(
+    score_obj,
+    data = titanic_subset,
+    outcome = "Survived"
+  )
+
+  exp.Pclass <- get_single_chisq(titanic_subset$Pclass, titanic_subset$Survived)
+  exp.Sex <- get_single_chisq(titanic_subset$Sex, titanic_subset$Survived)
   exp.Age <- NA
   exp.Fare <- NA
-  exp.Embarked <- get_single_chisq(data$Embarked, data$Survived)
+  exp.Embarked <- get_single_chisq(
+    titanic_subset$Embarked,
+    titanic_subset$Survived
+  )
 
   expect_true(tibble::is_tibble(score_res))
 
-  expect_identical(nrow(score_res), ncol(data) - 1L)
+  expect_identical(nrow(score_res), ncol(titanic_subset) - 1L)
 
   expect_named(score_res, c("name", "score", "outcome", "predictor"))
 
@@ -72,28 +88,37 @@ test_that("get_scores_cross_tab is working for chisq pval", {
 
 test_that("get_score_cross_tab is working for -log10(fisher pval)", {
   library(titanic)
+
   titanic_train <- titanic_train %>%
     dplyr::mutate(dplyr::across(c(Survived, Pclass, Sex, Embarked), as.factor))
-  data <- titanic_train %>%
+  titanic_subset <- titanic_train %>%
     dplyr::select(Survived, Pclass, Sex, Age, Fare, Embarked)
-  outcome <- "Survived"
-  score_obj <- score_cross_tab()
-  score_obj$score_type <- "pval_fisher"
-  score_obj$fdr <- FALSE
-  score_res <- get_scores_cross_tab(score_obj, data, outcome)
 
-  exp.Pclass <- get_single_fisher(data$Pclass, data$Survived)
+  score_obj <- score_cross_tab(score_type = "pval_fisher")
+  score_res <- get_scores_cross_tab(
+    score_obj,
+    data = titanic_subset,
+    outcome = "Survived"
+  )
+
+  exp.Pclass <- get_single_fisher(
+    titanic_subset$Pclass,
+    titanic_subset$Survived
+  )
   exp.Pclass <- -log10(exp.Pclass)
-  exp.Sex <- get_single_fisher(data$Sex, data$Survived)
+  exp.Sex <- get_single_fisher(titanic_subset$Sex, titanic_subset$Survived)
   exp.Sex <- -log10(exp.Sex)
   exp.Age <- NA
   exp.Fare <- NA
-  exp.Embarked <- get_single_fisher(data$Embarked, data$Survived)
+  exp.Embarked <- get_single_fisher(
+    titanic_subset$Embarked,
+    titanic_subset$Survived
+  )
   exp.Embarked <- -log10(exp.Embarked)
 
   expect_true(tibble::is_tibble(score_res))
 
-  expect_identical(nrow(score_res), ncol(data) - 1L)
+  expect_identical(nrow(score_res), ncol(titanic_subset) - 1L)
 
   expect_named(score_res, c("name", "score", "outcome", "predictor"))
 
@@ -109,26 +134,39 @@ test_that("get_score_cross_tab is working for -log10(fisher pval)", {
 
 test_that("get_score_cross_tab is working for fisher pval", {
   library(titanic)
+
   titanic_train <- titanic_train %>%
     dplyr::mutate(dplyr::across(c(Survived, Pclass, Sex, Embarked), as.factor))
-  data <- titanic_train %>%
+  titanic_subset <- titanic_train %>%
     dplyr::select(Survived, Pclass, Sex, Age, Fare, Embarked)
-  outcome <- "Survived"
-  score_obj <- score_cross_tab()
-  score_obj$score_type <- "pval_fisher"
-  score_obj$fdr <- FALSE
-  score_obj$neg_log_pval <- FALSE # Turn -log10() off
-  score_res <- get_scores_cross_tab(score_obj, data, outcome)
 
-  exp.Pclass <- get_single_fisher(data$Pclass, data$Survived)
-  exp.Sex <- get_single_fisher(data$Sex, data$Survived)
+  score_obj <- score_cross_tab(
+    score_type = "pval_fisher",
+    neg_log10 = FALSE,
+    direction = "minimize",
+    fallback_value = 0
+  )
+  score_res <- get_scores_cross_tab(
+    score_obj,
+    data = titanic_subset,
+    outcome = "Survived"
+  )
+
+  exp.Pclass <- get_single_fisher(
+    titanic_subset$Pclass,
+    titanic_subset$Survived
+  )
+  exp.Sex <- get_single_fisher(titanic_subset$Sex, titanic_subset$Survived)
   exp.Age <- NA
   exp.Fare <- NA
-  exp.Embarked <- get_single_fisher(data$Embarked, data$Survived)
+  exp.Embarked <- get_single_fisher(
+    titanic_subset$Embarked,
+    titanic_subset$Survived
+  )
 
   expect_true(tibble::is_tibble(score_res))
 
-  expect_identical(nrow(score_res), ncol(data) - 1L)
+  expect_identical(nrow(score_res), ncol(titanic_subset) - 1L)
 
   expect_named(score_res, c("name", "score", "outcome", "predictor"))
 

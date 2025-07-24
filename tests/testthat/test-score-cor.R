a92e532413d63b6c452ddffe91e33015ba9d8fec
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-07-24 13:00:43 -0700

    add test to score-cor

--- tests/testthat/test-score-cor.R
+++ tests/testthat/test-score-cor.R
@@ -1,129 +1,66 @@
-ames_subset <- helper_ames()
-
-ames_subset <- modeldata::ames |>
-  dplyr::select(
-    Sale_Price,
-    MS_SubClass,
-    MS_Zoning,
-    Lot_Frontage,
-    Lot_Area,
-    Street
+test_that("cor object creation", {
+  expect_s3_class(
+    score_cor_pearson,
+    c("filtro::class_score_cor", "filtro::class_score", "S7_object")
   )
 
-ames_cor_pearson_res <-
-  score_cor_pearson |>
-  fit(Sale_Price ~ ., data = ames_subset)
-ames_cor_pearson_res@results
-
-ames_cor_spearman_res <-
-  score_cor_spearman |>
-  fit(Sale_Price ~ ., data = ames_subset)
-ames_cor_spearman_res@results
-
-skip()
-
-test_that("get_scores_cor() is working for pearson", {
-  skip_if_not_installed("modeldata")
-
-  ames_subset <- helper_ames()
-  score_obj <- score_cor()
-  score_res <- get_scores_cor(
-    score_obj,
-    data = ames_subset,
-    outcome = "Sale_Price"
-  )
-
-  expect_true(tibble::is_tibble(score_res))
-
-  expect_identical(nrow(score_res), ncol(ames_subset) - 1L)
-
-  expect_named(score_res, c("name", "score", "outcome", "predictor"))
-
-  expect_equal(unique(score_res$name), "pearson")
-
-  expect_equal(unique(score_res$outcome), "Sale_Price")
-
-  exp.MS_SubClass <- NA
-
-  exp.MS_Zoning <- NA
-
-  exp.Lot_Frontage <- stats::cor(
-    ames_subset$Lot_Frontage,
-    ames_subset$Sale_Price,
-    method = "pearson"
-  )
-
-  exp.Lot_Area <- stats::cor(
-    ames_subset$Lot_Area,
-    ames_subset$Sale_Price,
-    method = "pearson"
-  )
-
-  exp.Street <- NA
-
-  expect_identical(
-    score_res$score,
-    c(
-      exp.MS_SubClass,
-      exp.MS_Zoning,
-      exp.Lot_Frontage,
-      exp.Lot_Area,
-      exp.Street
-    )
+  expect_s3_class(
+    score_cor_spearman,
+    c("filtro::class_score_cor", "filtro::class_score", "S7_object")
   )
 })
 
-# TODO Test Reversed stats::lm(x ~ y)
+test_that("cor computations", {
+  perm_pearson_res <-
+    score_cor_pearson |>
+    fit(mpg ~ ., data = mtcars)
 
-test_that("get_scores_cor() is working for spearman", {
-  skip_if_not_installed("modeldata")
+  perm_spearman_res <-
+    score_cor_spearman |>
+    fit(mpg ~ ., data = mtcars)
 
-  ames_subset <- helper_ames()
-  score_obj <- score_cor(score_type = "spearman")
-  score_res <- get_scores_cor(
-    score_obj,
-    data = ames_subset,
-    outcome = "Sale_Price"
-  )
+  # ----------------------------------------------------------------------------
 
-  expect_true(tibble::is_tibble(score_res))
+  predictors <- perm_pearson_res@results$predictor
+  for (predictor in predictors) {
+    pearson <- perm_pearson_res@results[
+      perm_pearson_res@results$predictor == predictor,
+    ]
+    spearman <- perm_spearman_res@results[
+      perm_spearman_res@results$predictor == predictor,
+    ]
 
-  expect_identical(nrow(score_res), ncol(ames_subset) - 1L)
+    tmp_data <- tibble::tibble(x = mtcars[[predictor]], y = mtcars$mpg)
+    pearson_exp <- stats::cor(tmp_data$x, tmp_data$y, method = "pearson")
+    spearman_exp <- stats::cor(tmp_data$x, tmp_data$y, method = "spearman")
 
-  expect_named(score_res, c("name", "score", "outcome", "predictor"))
+    expect_equal(pearson$score, pearson_exp)
+    expect_equal(spearman$score, spearman_exp)
+  }
 
-  expect_equal(unique(score_res$name), "spearman")
+  # ----------------------------------------------------------------------------
 
-  expect_equal(unique(score_res$outcome), "Sale_Price")
-
-  exp.MS_SubClass <- NA
-
-  exp.MS_Zoning <- NA
-
-  exp.Lot_Frontage <- stats::cor(
-    ames_subset$Lot_Frontage,
-    ames_subset$Sale_Price,
-    method = "spearman"
-  )
-
-  exp.Lot_Area <- stats::cor(
-    ames_subset$Lot_Area,
-    ames_subset$Sale_Price,
-    method = "spearman"
-  )
-
-  exp.Street <- NA
-
-  expect_identical(
-    score_res$score,
-    c(
-      exp.MS_SubClass,
-      exp.MS_Zoning,
-      exp.Lot_Frontage,
-      exp.Lot_Area,
-      exp.Street
-    )
-  )
+  expect_equal(perm_pearson_res@range, c(-1.0, 1.0))
+  expect_equal(perm_pearson_res@inclusive, rep(TRUE, 2))
+  expect_equal(perm_pearson_res@fallback_value, 1)
+  expect_equal(perm_pearson_res@direction, "maximize")
 })
 
-# TODO Test more after we add validators
+test_that("aov computations - wrong variable types", {
+  skip_if_not_installed("modeldata")
+
+  perm_data <- helper_perm()
+  perm_data <- perm_data |>
+    dplyr::mutate(dplyr::across(dplyr::starts_with("chem"), as.factor))
+
+  perm_pearson_res <-
+    score_cor_pearson |>
+    fit(permeability ~ ., data = perm_data)
+
+  expect_true(all(is.na(perm_pearson_res@results$score)))
+})
+
+test_that("aov computations - required packages", {
+  expect_equal(required_pkgs(score_cor_pearson), "filtro")
+  expect_equal(required_pkgs(score_cor_spearman), "filtro")
+})

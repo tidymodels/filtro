0c8b30dfee47f10d2d7ff8ba05b8b728358731ea
Frances Lin <franceslinyc@gmail.com>
2025-07-25 14:43:12 -0700

    Add more tests starting from forest imp (#87)
    * add test for score forest imp classification
    
    * add test for score forest imp regression
    
    * add test for score info gain class outcome
    
    * add test for score info gain numeric outcome
    
    * add test for score roc auc; now include
    class outcome, binary
    class outcome, multiclass
    numeric outcome, binary and multiclass predictors

--- tests/testthat/test-score-info_gain.R
+++ tests/testthat/test-score-info_gain.R
@@ -1,115 +1,157 @@
-# class outcome
-cells_subset <- modeldata::cells |>
-  dplyr::select(
-    class,
-    angle_ch_1,
-    area_ch_1,
-    avg_inten_ch_1,
-    avg_inten_ch_2,
-    avg_inten_ch_3
+test_that("object creation", {
+  expect_s3_class(
+    score_info_gain,
+    c("filtro::class_score_info_gain", "filtro::class_score", "S7_object")
   )
 
-cells_info_gain_res <- score_info_gain |>
-  fit(class ~ ., data = cells_subset)
-cells_info_gain_res@results
-
-cells_gain_ratio_res <- score_gain_ratio |>
-  fit(class ~ ., data = cells_subset)
-cells_gain_ratio_res@results
-
-cells_sym_uncert_res <- score_sym_uncert |>
-  fit(class ~ ., data = cells_subset)
-cells_sym_uncert_res@results
-
-# numeric outcome
-ames_subset <- modeldata::ames |>
-  dplyr::select(
-    Sale_Price,
-    MS_SubClass,
-    MS_Zoning,
-    Lot_Frontage,
-    Lot_Area,
-    Street
-  )
-ames_subset <- ames_subset |>
-  dplyr::mutate(Sale_Price = log10(Sale_Price))
-
-regression_task <- score_info_gain
-regression_task@mode <- "regression"
-
-ames_info_gain_regression_task_res <-
-  regression_task |>
-  fit(Sale_Price ~ ., data = ames_subset)
-ames_info_gain_regression_task_res@results
-
-skip()
-
-test_that("get_scores_info_gain() is working for classification", {
-  skip_if_not_installed("modeldata")
-
-  cells_subset <- modeldata::cells |>
-    dplyr::select(
-      class,
-      angle_ch_1,
-      area_ch_1,
-      avg_inten_ch_1,
-      avg_inten_ch_2
-    )
-
-  score_obj <- score_info_gain()
-  score_res <- get_scores_info_gain(
-    score_obj,
-    data = cells_subset,
-    outcome = "class"
+  expect_s3_class(
+    score_gain_ratio,
+    c("filtro::class_score_info_gain", "filtro::class_score", "S7_object")
   )
 
-  outcome <- "class"
-  y <- cells_subset[[outcome]]
-  X <- cells_subset[setdiff(names(cells_subset), outcome)]
-  fit <- FSelectorRcpp::information_gain(x = X, y = y)
-  exp.res <- fit$importance
-
-  expect_true(tibble::is_tibble(score_res))
-
-  expect_identical(nrow(score_res), ncol(cells_subset) - 1L)
-
-  expect_named(score_res, c("name", "score", "outcome", "predictor"))
-
-  expect_identical(score_res$score, exp.res)
-
-  expect_equal(unique(score_res$name), "infogain")
-
-  expect_equal(unique(score_res$outcome), "class")
+  expect_s3_class(
+    score_sym_uncert,
+    c("filtro::class_score_info_gain", "filtro::class_score", "S7_object")
+  )
 })
 
-test_that("get_scores_info_gain() is working for regression", {
+test_that("computations - class outcome", {
   skip_if_not_installed("modeldata")
+  cells_subset <- helper_cells()
 
-  ames_subset <- helper_ames()
-  score_obj <- score_info_gain(mode = "regression")
-  score_res <- get_scores_info_gain(
-    score_obj,
-    data = ames_subset,
-    outcome = "Sale_Price"
+  cells_info_gain_res <- score_info_gain |>
+    fit(class ~ ., data = cells_subset)
+
+  cells_gain_ratio_res <- score_gain_ratio |>
+    fit(class ~ ., data = cells_subset)
+
+  cells_sym_uncert_res <- score_sym_uncert |>
+    fit(class ~ ., data = cells_subset)
+
+  # ----------------------------------------------------------------------------
+
+  y <- cells_subset[["class"]]
+  X <- cells_subset[setdiff(names(cells_subset), "class")]
+  fit_infogain <- FSelectorRcpp::information_gain(
+    x = X,
+    y = y,
+    type = "infogain"
   )
+  imp_infogain <- fit_infogain$importance
+  imp_infogain[is.na(imp_infogain)] <- 0
 
-  outcome <- "Sale_Price"
-  y <- ames_subset[[outcome]]
-  X <- ames_subset[setdiff(names(ames_subset), outcome)]
-  fit <- FSelectorRcpp::information_gain(x = X, y = y, equal = TRUE)
-  exp.res <- fit$importance
+  fit_gainratio <- FSelectorRcpp::information_gain(
+    x = X,
+    y = y,
+    type = "gainratio"
+  )
+  imp_gainratio <- fit_gainratio$importance
+  imp_gainratio[is.na(imp_gainratio)] <- 0
 
-  expect_true(tibble::is_tibble(score_res))
+  fit_symuncert <- FSelectorRcpp::information_gain(
+    x = X,
+    y = y,
+    type = "symuncert"
+  )
+  imp_symuncert <- fit_symuncert$importance
+  imp_symuncert[is.na(imp_symuncert)] <- 0
 
-  expect_identical(nrow(score_res), ncol(ames_subset) - 1L)
+  expect_equal(cells_info_gain_res@results$score, imp_infogain)
+  expect_equal(cells_gain_ratio_res@results$score, imp_gainratio)
+  expect_equal(cells_sym_uncert_res@results$score, imp_symuncert)
 
-  expect_named(score_res, c("name", "score", "outcome", "predictor"))
+  # ----------------------------------------------------------------------------
 
-  expect_identical(score_res$score, exp.res)
+  expect_equal(cells_info_gain_res@range, c(0.0, Inf))
+  expect_equal(cells_info_gain_res@inclusive, rep(FALSE, 2))
+  expect_equal(cells_info_gain_res@fallback_value, Inf)
+  expect_equal(cells_info_gain_res@direction, "maximize")
 
-  expect_equal(unique(score_res$name), "infogain")
+  # ----------------------------------------------------------------------------
 
-  expect_equal(unique(score_res$outcome), "Sale_Price")
+  expect_equal(cells_gain_ratio_res@range, c(0.0, 1.0))
+  expect_equal(cells_gain_ratio_res@inclusive, rep(TRUE, 2))
+  expect_equal(cells_gain_ratio_res@fallback_value, 1.0)
+  expect_equal(cells_gain_ratio_res@direction, "maximize")
+})
+
+test_that("computations - numeric outcome", {
+  skip_if_not_installed("modeldata")
+  ames_subset <- helper_ames()
+  ames_subset <- ames_subset |>
+    dplyr::mutate(Sale_Price = log10(Sale_Price))
+
+  regression_task <- score_info_gain
+  regression_task@mode <- "regression"
+  ames_info_gain_res <- regression_task |>
+    fit(Sale_Price ~ ., data = ames_subset)
+
+  regression_task <- score_gain_ratio
+  regression_task@mode <- "regression"
+  ames_gain_ratio_res <- regression_task |>
+    fit(Sale_Price ~ ., data = ames_subset)
+
+  regression_task <- score_sym_uncert
+  regression_task@mode <- "regression"
+  ames_sym_uncert_res <- regression_task |>
+    fit(Sale_Price ~ ., data = ames_subset)
+
+  # ----------------------------------------------------------------------------
+
+  y <- ames_subset[["Sale_Price"]]
+  X <- ames_subset[setdiff(names(ames_subset), "Sale_Price")]
+  fit_infogain <- FSelectorRcpp::information_gain(
+    x = X,
+    y = y,
+    type = "infogain",
+    equal = TRUE
+  )
+  imp_infogain <- fit_infogain$importance
+  imp_infogain[is.na(imp_infogain)] <- 0
+
+  fit_gainratio <- FSelectorRcpp::information_gain(
+    x = X,
+    y = y,
+    type = "gainratio",
+    equal = TRUE
+  )
+  imp_gainratio <- fit_gainratio$importance
+  imp_gainratio[is.na(imp_gainratio)] <- 0
+
+  fit_symuncert <- FSelectorRcpp::information_gain(
+    x = X,
+    y = y,
+    type = "symuncert",
+    equal = TRUE
+  )
+  imp_symuncert <- fit_symuncert$importance
+  imp_symuncert[is.na(imp_symuncert)] <- 0
+
+  expect_equal(ames_info_gain_res@results$score, imp_infogain)
+  expect_equal(ames_gain_ratio_res@results$score, imp_gainratio)
+  expect_equal(ames_sym_uncert_res@results$score, imp_symuncert)
+
+  # ----------------------------------------------------------------------------
+
+  expect_equal(ames_info_gain_res@range, c(0.0, Inf))
+  expect_equal(ames_info_gain_res@inclusive, rep(FALSE, 2))
+  expect_equal(ames_info_gain_res@fallback_value, Inf)
+  expect_equal(ames_info_gain_res@direction, "maximize")
+
+  # ----------------------------------------------------------------------------
+
+  expect_equal(ames_gain_ratio_res@range, c(0.0, 1.0))
+  expect_equal(ames_gain_ratio_res@inclusive, rep(TRUE, 2))
+  expect_equal(ames_gain_ratio_res@fallback_value, 1.0)
+  expect_equal(ames_gain_ratio_res@direction, "maximize")
+})
+
+# TODO computations - wrong variable types
+
+test_that("computations - required packages", {
+  expect_equal(required_pkgs(score_info_gain), "filtro")
+  expect_equal(required_pkgs(score_gain_ratio), "filtro")
+  expect_equal(required_pkgs(score_sym_uncert), "filtro")
 })
 
 # TODO Test more after we add validators
--- tests/testthat/test-score-roc_auc.R
+++ tests/testthat/test-score-roc_auc.R
@@ -1,108 +1,141 @@
-# classification task
-cells_subset <- modeldata::cells |>
-  dplyr::select(
-    class,
-    angle_ch_1,
-    area_ch_1,
-    avg_inten_ch_1,
-    avg_inten_ch_2,
-    avg_inten_ch_3
+test_that("object creation", {
+  expect_s3_class(
+    score_roc_auc,
+    c("filtro::class_score_roc_auc", "filtro::class_score", "S7_object")
   )
-
-cells_roc_auc_res <- score_roc_auc |>
-  fit(class ~ ., data = cells_subset)
-cells_roc_auc_res@results
-
-# regression task
-ames_subset <- modeldata::ames |>
-  dplyr::select(
-    Sale_Price,
-    MS_SubClass,
-    MS_Zoning,
-    Lot_Frontage,
-    Lot_Area,
-    Street
-  )
-ames_subset <- ames_subset |>
-  dplyr::mutate(Sale_Price = log10(Sale_Price))
-
-ames_roc_auc_res <- score_roc_auc |>
-  fit(Sale_Price ~ ., data = ames_subset)
-ames_roc_auc_res@results
-
-skip()
-
-test_that("get_scores_roc_auc() is working", {
-  skip_if_not_installed("modeldata")
-
-  cells_subset <- modeldata::cells |>
-    dplyr::select(
-      class,
-      angle_ch_1,
-      area_ch_1,
-      avg_inten_ch_1,
-      avg_inten_ch_2
-    )
-
-  score_obj = score_roc_auc()
-  score_res <- get_scores_roc_auc(
-    score_obj,
-    data = cells_subset,
-    outcome = "class"
-  )
-
-  roc <- pROC::roc(
-    cells_subset$class,
-    cells_subset$angle_ch_1,
-    direction = "auto",
-    quiet = TRUE
-  )
-  exp.angle_ch_1 <- pROC::auc(roc) |> as.numeric()
-
-  roc <- pROC::roc(
-    cells_subset$class,
-    cells_subset$area_ch_1,
-    direction = "auto",
-    quiet = TRUE
-  )
-  exp.area_ch_1 <- pROC::auc(roc) |> as.numeric()
-
-  roc <- pROC::roc(
-    cells_subset$class,
-    cells_subset$avg_inten_ch_1,
-    direction = "auto",
-    quiet = TRUE
-  )
-  exp.avg_inten_ch_1 <- pROC::auc(roc) |> as.numeric()
-
-  roc <- pROC::roc(
-    cells_subset$class,
-    cells_subset$avg_inten_ch_2,
-    direction = "auto",
-    quiet = TRUE
-  )
-  exp.avg_inten_ch_2 <- pROC::auc(roc) |> as.numeric()
-
-  expect_true(tibble::is_tibble(score_res))
-
-  expect_identical(nrow(score_res), ncol(cells_subset) - 1L)
-
-  expect_named(score_res, c("name", "score", "outcome", "predictor"))
-
-  expect_identical(
-    score_res$score,
-    c(
-      exp.angle_ch_1,
-      exp.area_ch_1,
-      exp.avg_inten_ch_1,
-      exp.avg_inten_ch_2
-    )
-  )
-
-  expect_equal(unique(score_res$name), "roc_auc")
-
-  expect_equal(unique(score_res$outcome), "class")
 })
 
-# TODO Test pROC::multiclass.roc
+test_that("computations - class outcome, binary", {
+  skip_if_not_installed("modeldata")
+  cells_subset <- helper_cells()
+
+  cells_roc_auc_res <- score_roc_auc |>
+    fit(class ~ ., data = cells_subset)
+
+  # ----------------------------------------------------------------------------
+
+  predictors <- cells_roc_auc_res@results$predictor
+  for (predictor in predictors) {
+    tmp_data <- tibble::tibble(
+      y = cells_subset$class,
+      x = cells_subset[[predictor]]
+    )
+    roc <- pROC::roc(
+      tmp_data$y,
+      tmp_data$x,
+      direction = "auto",
+      quiet = TRUE
+    )
+    fit_roc_auc <- pROC::auc(roc) |> as.numeric()
+
+    roc_auc <- cells_roc_auc_res@results[
+      cells_roc_auc_res@results$predictor == predictor,
+    ]
+
+    expect_equal(roc_auc$score, fit_roc_auc)
+  }
+
+  # ----------------------------------------------------------------------------
+
+  expect_equal(cells_roc_auc_res@range, c(0.0, 1.0))
+  expect_equal(cells_roc_auc_res@inclusive, rep(TRUE, 2))
+  expect_equal(cells_roc_auc_res@fallback_value, 1.0)
+  expect_equal(cells_roc_auc_res@direction, "maximize")
+})
+
+test_that("computations - class outcome, multiclass", {
+  skip_if_not_installed("modeldata")
+  # Avoid dealing with NA in pROC::multiclass.roc(); Somehow try() doesn't quite work.
+  hpc_data <- helper_hpc_data() |> dplyr::select(-protocol, -day)
+
+  hpc_roc_auc_res <- score_roc_auc |>
+    fit(class ~ ., data = hpc_data)
+
+  # ----------------------------------------------------------------------------
+
+  predictors <- hpc_roc_auc_res@results$predictor
+  for (predictor in predictors) {
+    tmp_data <- tibble::tibble(
+      y = hpc_data$class,
+      x = hpc_data[[predictor]]
+    )
+    roc <- pROC::multiclass.roc(
+      tmp_data$y,
+      tmp_data$x,
+      direction = "auto",
+      quiet = TRUE
+    )
+    fit_roc_auc <- pROC::auc(roc) |> as.numeric()
+
+    roc_auc <- hpc_roc_auc_res@results[
+      hpc_roc_auc_res@results$predictor == predictor,
+    ]
+
+    expect_equal(roc_auc$score, fit_roc_auc)
+  }
+
+  # ----------------------------------------------------------------------------
+
+  expect_equal(hpc_roc_auc_res@range, c(0.0, 1.0))
+  expect_equal(hpc_roc_auc_res@inclusive, rep(TRUE, 2))
+  expect_equal(hpc_roc_auc_res@fallback_value, 1.0)
+  expect_equal(hpc_roc_auc_res@direction, "maximize")
+})
+
+test_that("computations - numeric outcome, binary and multiclass predictors", {
+  skip_if_not_installed("modeldata")
+  # Avoid dealing with NA in pROC::multiclass.roc(); Somehow try() doesn't quite work.
+  ames_subset <- helper_ames() |> dplyr::select(-Lot_Frontage, -Lot_Area)
+  ames_subset <- ames_subset |>
+    dplyr::mutate(Sale_Price = log10(Sale_Price))
+
+  ames_roc_auc_res <- score_roc_auc |>
+    fit(Sale_Price ~ ., data = ames_subset)
+
+  # ----------------------------------------------------------------------------
+
+  predictors <- ames_roc_auc_res@results$predictor
+  for (predictor in predictors) {
+    tmp_data <- tibble::tibble(
+      y = ames_subset[[predictor]],
+      x = ames_subset$Sale_Price
+    )
+    if (length(levels(tmp_data$y)) == 2) {
+      roc <- pROC::roc(
+        tmp_data$y,
+        tmp_data$x,
+        direction = "auto",
+        quiet = TRUE
+      )
+    } else {
+      roc <- pROC::multiclass.roc(
+        tmp_data$y,
+        tmp_data$x,
+        direction = "auto",
+        quiet = TRUE
+      )
+    }
+    fit_roc_auc <- pROC::auc(roc) |> as.numeric()
+
+    roc_auc <- ames_roc_auc_res@results[
+      ames_roc_auc_res@results$predictor == predictor,
+    ]
+
+    expect_equal(roc_auc$score, fit_roc_auc)
+  }
+
+  # ----------------------------------------------------------------------------
+
+  expect_equal(ames_roc_auc_res@range, c(0.0, 1.0))
+  expect_equal(ames_roc_auc_res@inclusive, rep(TRUE, 2))
+  expect_equal(ames_roc_auc_res@fallback_value, 1.0)
+  expect_equal(ames_roc_auc_res@direction, "maximize")
+})
+
+# TODO computations - wrong variable types
+
+test_that("computations - required packages", {
+  expect_equal(required_pkgs(score_roc_auc), "filtro")
+})
+
 # TODO Test more after we add validators

6e5301d30920abbbc564b799a9174cae3d8c3378
topepo <mxkuhn@gmail.com>
2025-07-25 10:34:33 -0400

    minor linting

--- R/desirability2.R
+++ R/desirability2.R
@@ -39,6 +39,7 @@ show_best_desirability_prop <- function(
 }
 
 # ------------------------------------------------------------------------------
+
 show_best_desirability_num <- function(
   x,
   ...,
@@ -60,6 +61,7 @@ show_best_desirability_num <- function(
 }
 
 # ------------------------------------------------------------------------------
+
 show_best_desirability_cutoff <- function(
   x,
   ...,
@@ -115,52 +117,3 @@ make_col_names <- function(x) {
   res <- purrr::map2_chr(fns, vars, ~ paste0(".", .x, "_", .y))
   make.names(res, unique = TRUE)
 }
-
-# # ------------------------------------------------------------------------------
-# show_best_score_dual <- function(
-#   x,
-#   ...,
-#   prop_terms = 0.99,
-#   cutoff = NULL,
-#   eval_time = NULL
-# ) {
-#   mtr <- x
-#   all_vars <- names(mtr)
-
-#   res <- desirability2::desirability(..., .use_data = TRUE)
-
-#   d_vars <- sort(unique(unlist(res@variables)))
-#   extra_vars <- setdiff(d_vars, all_vars)
-#   num_extras <- length(extra_vars)
-#   if (num_extras > 0) {
-#     cli::cli_abort(
-#       "The desirability specification includes {num_extras} variable{?s} that
-#       {?was/were} not collected: {.val {extra_vars}}."
-#     )
-#   }
-
-#   # Make individual scores:
-#   d_nms <- make_col_names(res)
-#   for (i in seq_along(res@translated)) {
-#     tmp <- try(rlang::eval_tidy(res@translated[[i]], mtr), silent = TRUE)
-#     if (inherits(tmp, "try-error")) {
-#       cli::cli_abort(
-#         "An error occured when computing a desirability score: {tmp}."
-#       )
-#     }
-#     mtr[[d_nms[i]]] <- tmp
-#   }
-
-#   mtr <-
-#     mtr |>
-#     dplyr::mutate(
-#       .d_overall = d_overall(dplyr::across(dplyr::starts_with(".d_")))
-#     ) |>
-#     dplyr::slice_max(.d_overall, prop = prop_terms, with_ties = TRUE)
-
-#   if (!is.null(cutoff)) {
-#     mtr <- mtr |> dplyr::filter(.d_overall >= cutoff) # COMMENT Doesn't call cutoff_selected
-#   }
-
-#   mtr
-# }

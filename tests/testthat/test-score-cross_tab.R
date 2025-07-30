7a918e9b6f6bfbdaf93c3e7d95574e40afd1f88b
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-07-30 13:26:54 -0700

    Add test to test multiclass outcome

--- tests/testthat/test-score-cross_tab.R
+++ tests/testthat/test-score-cross_tab.R
@@ -108,11 +108,58 @@ test_that("computations fisher", {
 })
 
 # TODO Test fdr
-# TODO Test multiclass
 
-skip()
+test_that("computations chisq test - multiclass outcome", {
+  skip_if_not_installed("tidymodels")
 
-library(tidymodels)
-hpc_subset <- helper_hpc_data()
+  hpc_subset <- helper_hpc_data()
+
+  set.seed(42)
+  hpc_xtab_pval_chisq_res <- score_xtab_pval_chisq |>
+    fit(class ~ ., data = hpc_subset)
+
+  natrual_units <- score_xtab_pval_chisq |> dont_log_pvalues()
+
+  set.seed(42)
+  hpc_xtab_pval_natrual_res <-
+    natrual_units |>
+    fit(class ~ ., data = hpc_subset)
+
+  # ----------------------------------------------------------------------------
+
+  predictors <- hpc_xtab_pval_chisq_res@results$predictor
+  for (predictor in predictors) {
+    chisq <- hpc_xtab_pval_chisq_res@results[
+      hpc_xtab_pval_chisq_res@results$predictor == predictor,
+    ]
+    nat <- hpc_xtab_pval_natrual_res@results[
+      hpc_xtab_pval_natrual_res@results$predictor == predictor,
+    ]
+
+    tmp_data <- tibble::tibble(
+      x = hpc_subset[[predictor]],
+      y = hpc_subset$class
+    )
+
+    if (is.numeric(tmp_data$x)) {
+      fit_chisq <- NA_real_
+    } else {
+      set.seed(42)
+      tmp_tab <- table(tmp_data$x, sample(tmp_data$y))
+
+      fit_chisq <- try(
+        suppressWarnings(stats::chisq.test(tmp_tab)$p.value),
+        silent = TRUE
+      )
+
+      if (inherits(fit_chisq, "try-error")) {
+        fit_chisq <- NA_real_
+      }
+    }
+
+    expect_equal(chisq$score, -log10(fit_chisq))
+    expect_equal(nat$score, fit_chisq)
+  }
+})
 
 # TODO Test more after we add validators

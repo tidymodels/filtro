ca7163f984d71b9179b8f199f8c228c492e02f5e
Frances Lin <franceslinyc@gmail.com>
2025-08-01 09:40:22 -0700

    Remove scales (#104)

--- R/misc.R
+++ R/misc.R
@@ -1,21 +1,3 @@
-#' Absolute Transformation
-#'
-#' @export
-transform_abs <- scales::trans_new(
-  name = "abs",
-  transform = function(x) abs(x),
-  inverse = function(x) x # dummy inverse
-)
-
-#' Negative log10 Transformation TODO Rewrite using other method
-#'
-#' @export
-transform_neg_log10 <- scales::trans_new(
-  name = "neg-log-10",
-  transform = function(x) -log10(x),
-  inverse = function(x) 10^(-x)
-)
-
 #' @noRd
 #' @keywords internal
 check_target <- function(target) {

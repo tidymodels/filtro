e64631087094efd139b429c77d5842df6f71d13c
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-08-05 13:40:35 -0700

    usethis::use_data()

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Ames exampled score results
+#'
+#' @name ames_scores_results
+#' @aliases ames_scores_results
+#' @docType data
+#' @return A tibble
+#' @examples
+#' data(ames_scores_results)
+NULL

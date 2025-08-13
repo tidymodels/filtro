142ad6ab1ddc7df205f214a6235a9d4cea0ab0fe
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-08-13 08:56:41 -0700

    Add tests and TODO

--- tests/testthat/test-desirability2.R
+++ tests/testthat/test-desirability2.R
@@ -1,4 +1,4 @@
-test_that("good results", {
+test_that("good results - based on prop", {
   skip_if_not_installed("desirability2")
   library(desirability2) # Otherwise Error in `compute_desirability_scores(res, mtr = mtr)`
 
@@ -6,13 +6,29 @@ test_that("good results", {
 
   des_1 <- ames_scores_results |>
     show_best_desirability_prop(
-      maximize(cor_pearson, low = 0, high = 1)
+      maximize(cor_pearson, low = 0, high = 1),
+      maximize(imp_rf),
+      maximize(infogain)
     )
 
-  mtr <- des_1
+  # ----------------------------------------------------------------------------
 
+  mtr <- des_1
   d_cor_pearson_1 <- desirability2::d_max(mtr$cor_pearson, low = 0, high = 1)
+  d_imp_rf_1 <- desirability2::d_max(
+    mtr$imp_rf,
+    low = min(mtr$imp_rf),
+    high = max(mtr$imp_rf)
+  )
+  d_infogain_1 <- desirability2::d_max(
+    mtr$infogain,
+    low = min(mtr$infogain),
+    high = max(mtr$infogain)
+  )
+
   expect_equal(des_1$.d_max_cor_pearson, d_cor_pearson_1)
+  expect_equal(des_1$.d_max_imp_rf, d_imp_rf_1)
+  expect_equal(des_1$.d_max_infogain, d_infogain_1)
 
   expect_named(
     des_1,
@@ -23,82 +39,60 @@ test_that("good results", {
       "imp_rf",
       "infogain",
       ".d_max_cor_pearson",
+      ".d_max_imp_rf",
+      ".d_max_infogain",
       ".d_overall"
     )
   )
 })
 
-skip()
+test_that("good results - based on num", {
+  skip_if_not_installed("desirability2")
+  library(desirability2) # Otherwise Error in `compute_desirability_scores(res, mtr = mtr)`
 
-library(filtro)
-utils::data("ames_scores_results", package = "filtro")
-ames_scores_results <- ames_scores_results |>
-  dplyr::select(-outcome)
-ames_scores_results
+  ames_scores_results <- helper_ames_res()
 
-# show_best_desirability_prop
-library(desirability2)
+  des_1 <- ames_scores_results |>
+    show_best_desirability_num(
+      maximize(cor_pearson, low = 0, high = 1),
+      maximize(imp_rf),
+      maximize(infogain)
+    )
 
-ames_scores_results |>
-  show_best_desirability_prop(
-    maximize(cor_pearson, low = 0, high = 1)
+  # ----------------------------------------------------------------------------
+
+  mtr <- des_1
+  d_cor_pearson_1 <- desirability2::d_max(mtr$cor_pearson, low = 0, high = 1)
+  d_imp_rf_1 <- desirability2::d_max(
+    mtr$imp_rf,
+    low = min(mtr$imp_rf),
+    high = max(mtr$imp_rf)
+  )
+  d_infogain_1 <- desirability2::d_max(
+    mtr$infogain,
+    low = min(mtr$infogain),
+    high = max(mtr$infogain)
   )
 
-ames_scores_results |>
-  show_best_desirability_prop(
-    maximize(cor_pearson, low = 0, high = 1),
-    maximize(imp_rf)
-  )
+  expect_equal(des_1$.d_max_cor_pearson, d_cor_pearson_1)
+  expect_equal(des_1$.d_max_imp_rf, d_imp_rf_1)
+  expect_equal(des_1$.d_max_infogain, d_infogain_1)
 
-ames_scores_results |>
-  show_best_desirability_prop(
-    maximize(cor_pearson, low = 0, high = 1),
-    maximize(imp_rf),
-    maximize(infogain)
+  expect_named(
+    des_1,
+    c(
+      "predictor",
+      "aov_pval",
+      "cor_pearson",
+      "imp_rf",
+      "infogain",
+      ".d_max_cor_pearson",
+      ".d_max_imp_rf",
+      ".d_max_infogain",
+      ".d_overall"
+    )
   )
+})
 
-ames_scores_results |>
-  show_best_desirability_prop(
-    maximize(cor_pearson, low = 0, high = 1),
-    maximize(imp_rf),
-    maximize(infogain),
-    prop_terms = 0.2
-  )
-
-ames_scores_results |>
-  show_best_desirability_prop(
-    target(cor_pearson, low = 0.2, target = 0.255, high = 0.9)
-  )
-
-ames_scores_results |>
-  show_best_desirability_prop(
-    constrain(cor_pearson, low = 0.2, high = 1)
-  )
-
-# show_best_desirability_num
-
-ames_scores_results |>
-  show_best_desirability_num(
-    maximize(cor_pearson, low = 0, high = 1)
-  )
-
-ames_scores_results |>
-  show_best_desirability_num(
-    maximize(cor_pearson, low = 0, high = 1),
-    maximize(imp_rf)
-  )
-
-ames_scores_results |>
-  show_best_desirability_num(
-    maximize(cor_pearson, low = 0, high = 1),
-    maximize(imp_rf),
-    maximize(infogain)
-  )
-
-ames_scores_results |>
-  show_best_desirability_num(
-    maximize(cor_pearson, low = 0, high = 1),
-    maximize(imp_rf),
-    maximize(infogain),
-    num_terms = 2
-  )
+# TODO Test missing data
+# TODO Test bad results

094cc0786c0a67a4bec20ad5a7d7d4b4af07be72
Frances Lin <37535633+franceslinyc@users.noreply.github.com>
2025-08-15 10:52:17 -0700

    Doc exampled score results

--- R/data.R
+++ R/data.R
@@ -1,10 +1,14 @@
 #' Ames exampled score results
 #'
+#' This data an exampled score results for ames. See
+#' [show_best_desirability_prop()] for how it is used
+#' for demonstration, and see [fill_safe_values()]
+#' for how it is created.
+#'
 #' @name ames_scores_results
 #' @aliases ames_scores_results
 #' @docType data
-#' @return A tibble
+#' @return A data frame of exampled score results.
 #' @examples
 #' data(ames_scores_results)
-#' ames_scores_results
 NULL
